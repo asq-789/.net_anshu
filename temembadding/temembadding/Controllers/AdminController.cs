2d605e9a8f8323edf4fee83700adc34e47189508
asq-789 <130150313+asq-789@users.noreply.github.com>
2024-07-04 11:01:58 +0500

    dedw

---
+++ temembadding/temembadding/Controllers/AdminController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace temembadding.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string email, string password)
+        {
+            if(email == "admin@gmail.com" && password == "123" )
+            {
+                HttpContext.Session.SetString("adminemail", email);
+                HttpContext.Session.SetString("role","admin");
+                return RedirectToAction("Index");
+
+            }
+          else if(email == "user@gmail.com" && password == "123")
+            {
+
+                HttpContext.Session.SetString("useremail", email);
+                HttpContext.Session.SetString("role", "user");
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public IActionResult AdminLogout()
+        {
+            HttpContext.Session.Clear();
+            HttpContext.Session.Remove("adminemail");
+            HttpContext.Session.Remove("role");
+            return RedirectToAction("Login");
+        }
+        public IActionResult UserLogout()
+        {
+            HttpContext.Session.Clear();
+            HttpContext.Session.Remove("useremail");
+            HttpContext.Session.Remove("role");
+            return RedirectToAction("Login");
+        }
+    }
+
+}
---
+++ temembadding/temembadding/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+using Microsoft.AspNetCore.Mvc;
+
+using temembadding.Models;
+
+namespace temembadding.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+           if (HttpContext.Session.GetString ("role")== "user")
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login","Admin");
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        
+    }
+}
